4d420b0a932df17266ce12b3f314073caee3f461
Anurag rajput <anuragrajput.work@gmail.com>
2026-01-13 16:59:05 +0530

    Bg-colourChanger

---
+++ Bg-colourChnager/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+function App() {
+  let [bgColor, setBgColor] = useState('grey')
+
+  return (
+    <>
+      <div className="w-full h-screen duration-200" style={{ backgroundColor: bgColor }}> 
+
+        <div className="fixed bottom-12 inset-x-0 flex justify-center px-4">
+          <div className="flex gap-3 rounded-full bg-white/80 backdrop-blur-md px-4 py-3 shadow-2xl border border-white/30">
+
+            <button onClick={() => setBgColor('red')} 
+            className="px-5 py-2 rounded-full text-white font-medium 
+      bg-red-500 hover:bg-red-600 
+      hover:scale-105 transition-all duration-200 shadow-md">
+              Red
+            </button>
+
+            <button onClick={() => setBgColor('green')} 
+            className="px-5 py-2 rounded-full text-white font-medium 
+      bg-green-500 hover:bg-green-600 
+      hover:scale-105 transition-all duration-200 shadow-md">
+              Green
+            </button>
+
+            <button onClick={() => setBgColor('blue')} 
+            className="px-5 py-2 rounded-full text-white font-medium 
+      bg-blue-500 hover:bg-blue-600 
+      hover:scale-105 transition-all duration-200 shadow-md">
+              Blue
+            </button>
+
+          </div>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
